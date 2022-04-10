4a5b1de41702ea1d8abf221fb37aaa59cdc7a27c
SharmaManjul <manjul199912@gmail.com>
2022-04-10 11:50:13 -0500

    Removing bowser icon.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,9 @@
 import Theme from '../styles/theme';
 
-const prefix = '/MyPortfolio'
-
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
-        <link rel="shortcut icon" href={prefix+'/favicon.ico'}/>
         <Component {...pageProps} />
       </Theme>
     </>

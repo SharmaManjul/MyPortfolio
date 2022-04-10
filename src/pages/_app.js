f7eedd262a647411bbad794af828392dc0838ff6
SharmaManjul <manjul199912@gmail.com>
2022-04-10 11:47:43 -0500

    Adding bowser icon.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
 import Theme from '../styles/theme';
 
+const prefix = '/MyPortfolio'
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Theme>
+        <link rel="shortcut icon" href={prefix+'/favicon.ico'}/>
         <Component {...pageProps} />
       </Theme>
     </>
